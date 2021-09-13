0b056965ac6b4e647d41ceb441fb24281e28f064
Gabriel Haim <gabriel.haim@dominodatalab.com>
2021-09-13 11:48:54 -0700

    Refactor authentication classes

---
+++ domino_data_sdk/auth.py
@@ -0,0 +1,83 @@
+"""Authentication classes for HTTP and Flight clients."""
+
+from typing import Dict, Optional
+
+import os
+
+import attr
+from pyarrow import flight
+
+from datasource_api_client.client import Client
+
+
+@attr.s(auto_attribs=True)
+class AuthenticatedClient(Client):
+    """A client that authenticate all requests with either the API Key or JWT."""
+
+    api_key: Optional[str] = attr.ib()
+    token_file: Optional[str] = attr.ib()
+
+    def __attrs_post_init__(self):
+        if not (self.api_key or self.token_file):
+            raise Exception(
+                "One of two authentication methods needs to be supplied "
+                "(API Key or JWT Location)"
+            )
+
+    def get_headers(self) -> Dict[str, str]:
+        """Get headers with both JWT and API Key."""
+        if self.api_key:
+            return {"X-Domino-Api-Key": self.api_key, **self.headers}
+
+        if self.token_file:
+            with open(self.token_file, encoding="locale") as token_file:
+                jwt = token_file.readline().rstrip()
+            return {"Authorization": f"Bearer {jwt}", **self.headers}
+
+        return self.headers
+
+
+@attr.s(auto_attribs=True)
+class AuthMiddlewareFactory(flight.ClientMiddlewareFactory):
+    """Middleware Factory for authenticating flight requests."""
+
+    # pylint: disable=too-few-public-methods
+    api_key: Optional[str] = attr.ib()
+    token_file: Optional[str] = attr.ib()
+
+    def __attrs_post_init__(self):
+        if not (self.api_key or self.token_file):
+            raise Exception(
+                "One of two authentication methods needs to be supplied "
+                "(API Key or JWT Location)"
+            )
+
+    def start_call(self, info):  # pylint: disable=unused-argument
+        """Called at the start of an RPC."""
+        jwt = None
+
+        if self.token_file is not None:
+            with open(self.token_file, encoding="ascii") as token_file:
+                jwt = token_file.readline().rstrip()
+
+        return AuthMiddleware(self.api_key, jwt)
+
+
+@attr.s(auto_attribs=True)
+class AuthMiddleware(flight.ClientMiddleware):
+    """Middleware for authenticating flight requests."""
+
+    # pylint: disable=too-few-public-methods
+    api_key: Optional[str] = attr.ib()
+    jwt: Optional[str] = attr.ib()
+
+    def sending_headers(self):
+        """Return authentication headers."""
+        headers = {}
+
+        if self.api_key is not None:
+            headers["X-Domino-Api-Key"] = self.api_key
+        if self.jwt is not None:
+            headers["X-Domino-Jwt"] = self.jwt
+
+        return headers
