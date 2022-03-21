b44ebb8c8881e1f94aa2d5fbcec09bbd473d87be
Murat Cetin <68244282+ddl-mcetin@users.noreply.github.com>
2022-03-21 15:24:24 -0700

    latest changes from murat

--- domino_data/featurestore.py
+++ domino_data/featurestore.py
@@ -1,6 +1,8 @@
 import datetime
+import os
 import uuid
 
+import bson
 import click
 # from pathlib import Path
 from datetime import datetime
@@ -10,6 +12,7 @@ from feature_store_api_client.api.default import post_feature_store_name
 from feature_store_api_client.models import CreateFeatureStoreRequest, \
     BatchSource, BatchSourceSourceOptions, Feature, \
     Entity, FeatureView, FeatureViewTags, FeatureStore as DominoFeatureStore
+import pytz
 
 
 @click.command()
@@ -18,6 +21,7 @@ from feature_store_api_client.models import CreateFeatureStoreRequest, \
 def domino_sync(name):
     """Sync information in registry.db with Domino"""
     # repo = Path.cwd()
+    # TODO - read base_url proper
     domino = AuthenticatedClient(
         base_url="https://mcetin11886.workbench-accessdata-team-sandbox.domino.tech/featurestore",
         api_key="fba753233ce044fe6264d4c96899bff83223f1b28fe251c5b5786f73123cb64d",
@@ -25,6 +29,7 @@ def domino_sync(name):
         headers={"Accept": "application/json"},
     )
 
+    # TODO - read repo from cwd or input
     repo = "/Users/muratcetin/github.com/real-time-credit-scoring-on-aws-tutorial/feature_repo/"
     fs = FeatureStore(repo)
     fvs = fs.list_feature_views()
@@ -47,14 +52,14 @@ def domino_sync(name):
         ) for fv in fvs]
 
     # TODO - read this from the project proper
-    domino_project_id = uuid.UUID
+    domino_project_id = os.getenv("DOMINO_RUN_ID", "6238f45cf6d751a45f6d0539")
 
     feature_store = DominoFeatureStore(
-        id=str(uuid.UUID),
+        id=str(bson.ObjectId()),
         project_id=str(domino_project_id),
         name=name,
         feature_views=dfvs,
-        creation_time=datetime.now(),
+        creation_time=datetime.now(pytz.utc),
         description=f"Feast feature store: {name}",
     )
     fs_request = CreateFeatureStoreRequest(
@@ -68,7 +73,7 @@ def domino_sync(name):
         client=domino,
         json_body=fs_request,
     )
-    print(f"feature views = {dfvs}")
+    print(f"got response: {response}")
 
 
 if __name__ == '__main__':
