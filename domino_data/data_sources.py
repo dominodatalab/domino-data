c7149f8d556f7e7959c76e828dfb149d3da79c37
ddl-catherinetu <69922646+ddl-catherinetu@users.noreply.github.com>
2022-03-25 13:01:51 -0400

    IAM Credential prop SDK support for session (#38)
    * redshift cred proop
    
    * add session
    
    * fix code style things

--- domino_data/data_sources.py
+++ domino_data/data_sources.py
@@ -81,6 +81,7 @@ class CredElem(Enum):
     PASSWORD = "password"
     USERNAME = "username"
     TOKEN = "token"
+    SESSION = "session"
 
 
 def _cred(elem: CredElem) -> Any:
@@ -208,6 +209,10 @@ class RedshiftConfig(Config):
 
     password: Optional[str] = _cred(elem=CredElem.PASSWORD)
     username: Optional[str] = _cred(elem=CredElem.USERNAME)
+
+    aws_access_key_id: Optional[str] = _cred(elem=CredElem.USERNAME)
+    aws_secret_access_key: Optional[str] = _cred(elem=CredElem.PASSWORD)
+    session: Optional[str] = _cred(elem=CredElem.SESSION)
 
 
 @attr.s(auto_attribs=True)
@@ -460,10 +465,7 @@ class Datasource:
             # TODO: grab location from meta
             location = "DOMINO_TOKEN_FILE"
             credentials = self._load_oauth_token(location)
-        elif (
-            self.datasource_type == DatasourceDtoDataSourceType.S3CONFIG.value
-            and self.auth_type == DatasourceDtoAuthType.AWSIAMROLE.value
-        ):
+        elif self.auth_type == DatasourceDtoAuthType.AWSIAMROLE.value:
             # TODO: grab location from meta
             location = "AWS_SHARED_CREDENTIALS_FILE"
             credentials = self._load_aws_credentials(location)
@@ -494,6 +496,7 @@ class Datasource:
         return dict(
             username=aws_config.get(profile, "aws_access_key_id"),
             password=aws_config.get(profile, "aws_secret_access_key"),
+            session=aws_config.get(profile, "aws_session_token"),
         )
 
     def update(self, config: DatasourceConfig) -> None:
