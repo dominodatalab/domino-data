c1c58731bcc30d9c8a31ae12b3f59a843201e892
ddl-joyce-zhao <joyce.zhao@dominodatalab.com>
2022-11-10 11:54:08 -0500

    Reverted the changes to data sources
    The changes to data sources were introduced by mistake during rebasing. Reverting them.

--- datasource_api_client/api/proxy/get_key_url.py
+++ datasource_api_client/api/proxy/get_key_url.py
@@ -13,7 +13,7 @@ def _get_kwargs(
     client: Client,
     json_body: KeyRequest,
 ) -> Dict[str, Any]:
-    url = "{}/objectstore/key".format(client.base_url)
+    url = f"{client.base_url}/objectstore/key"
 
     headers: Dict[str, str] = client.get_headers()
     cookies: Dict[str, Any] = client.get_cookies()
--- datasource_api_client/api/proxy/log_metric.py
+++ datasource_api_client/api/proxy/log_metric.py
@@ -15,7 +15,7 @@ def _get_kwargs(
     b: int,
     m: LogMetricM,
 ) -> Dict[str, Any]:
-    url = "{}/objectstore/metric".format(client.base_url)
+    url = f"{client.base_url}/objectstore/metric"
 
     headers: Dict[str, str] = client.get_headers()
     cookies: Dict[str, Any] = client.get_cookies()
