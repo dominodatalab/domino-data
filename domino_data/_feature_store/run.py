b67bfb4e58a36a09355fea50dc2b28ccca65a9bb
ddl-joyce-zhao <joyce.zhao@dominodatalab.com>
2022-11-10 11:01:56 -0500

    Addressed review comments

--- domino_data/_feature_store/run.py
+++ domino_data/_feature_store/run.py
@@ -6,33 +6,34 @@ from ..logging import logger
 from .sync import feature_store_sync
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run feature store sync.")
+    parser.add_argument(
+        "-i",
+        "--id",
+        metavar="FEATURE_STORE_ID",
+        type=str,
+        required=True,
+        help="the domino feature store id",
+    )
+    parser.add_argument(
+        "-p",
+        "--path",
+        metavar="FEAST_REPO_PATH",
+        type=str,
+        required=False,
+        help="the feature store git repo path",
+    )
+    parser.add_argument(
+        "-b",
+        "--branch",
+        metavar="FEAST_REPO_BRANCH",
+        type=str,
+        required=False,
+        help="the feature store git repo branch",
+    )
+    args = parser.parse_args()
+
     try:
-        parser = argparse.ArgumentParser(description="Run feature store sync.")
-        parser.add_argument(
-            "-i",
-            "--id",
-            metavar="FEATURE_STORE_ID",
-            type=str,
-            required=True,
-            help="the domino feature store id",
-        )
-        parser.add_argument(
-            "-p",
-            "--path",
-            metavar="FEAST_REPO_PATH",
-            type=str,
-            required=False,
-            help="the feature store git repo path",
-        )
-        parser.add_argument(
-            "-b",
-            "--branch",
-            metavar="FEAST_REPO_BRANCH",
-            type=str,
-            required=False,
-            help="the feature store git repo branch",
-        )
-        args = parser.parse_args()
         feature_store_sync(args.id, args.path, args.branch)
     except Exception as e:
         logger.exception(e)
