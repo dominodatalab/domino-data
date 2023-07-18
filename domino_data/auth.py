1e7fcb99d386af94c78de5c716b51a39aa973459
ddl-giuliocapolino <giulio.capolino@dominodatalab.com>
2023-07-19 00:27:45 +0200

    iteration on comments + add run id to http requests

--- domino_data/auth.py
+++ domino_data/auth.py
@@ -69,9 +69,14 @@ class AuthenticatedClient(Client):
 class ProxyClient(AuthenticatedClient):
     """A client that authenticates all requests but with Proxy headers."""
 
+    run_id: Optional[str] = attr.ib()
+
     def get_headers(self) -> Dict[str, str]:
         if self.api_key:
             self.headers["X-Domino-Api-Key"] = self.api_key
+
+        if self.run_id:
+            self.headers["X-Domino-Run-Id"] = self.run_id
 
         if self.token_url is not None:
             try:
