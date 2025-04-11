031a90d8c2225b29716b9479f3d8a2393e5f1d90
ddl-joyce-zhao <107629651+ddl-joyce-zhao@users.noreply.github.com>
2025-04-10 21:54:12 -0400

    dropped the support for connecting domino pinecone data source using pinecone python 2.x client (#157)
    * dropped the pinecone 2.x client support
    
    * fixed the test
    
    * Update vectordb.py
    
    * fixed the warning
    
    * Update vectordb.py

--- domino_data/vectordb.py
+++ domino_data/vectordb.py
@@ -9,58 +9,15 @@ _import_error_message = (
 )
 
 try:
-    from pinecone.core.client.configuration import Configuration as OpenApiConfiguration
+    import pinecone  # noqa: F401
 except ImportError as e:
-    if e.msg == "No module named 'pinecone'":
+    if e.msg == "No module named 'pinecone-client'":
         raise ImportError(_import_error_message) from e
     else:
         raise
 
 HEADER_DOMINO_DATASOURCE = "X-Domino-Datasource"
 HEADER_PINECONE_INDEX = "X-Domino-Pinecone-Index"
-
-
-class DominoPineconeConfiguration(OpenApiConfiguration):
-    def __init__(
-        self,
-        datasource=None,
-        host=None,
-        api_key=None,
-        api_key_prefix=None,
-        access_token=None,
-        username=None,
-        password=None,
-        discard_unknown_keys=False,
-        disabled_client_side_validations="",
-        server_index=None,
-        server_variables=None,
-        server_operation_index=None,
-        server_operation_variables=None,
-        ssl_ca_cert=None,
-    ):
-
-        super().__init__(
-            host,
-            api_key,
-            api_key_prefix,
-            access_token,
-            username,
-            password,
-            discard_unknown_keys,
-            disabled_client_side_validations,
-            server_index,
-            server_variables,
-            server_operation_index,
-            server_operation_variables,
-            ssl_ca_cert,
-        )
-
-        self.proxy = os.getenv("DOMINO_DATA_API_GATEWAY", "http://127.0.0.1:8766")
-        self.proxy_headers = {HEADER_DOMINO_DATASOURCE: datasource}
-
-    def get_host_from_settings(self, index, variables=None, servers=None):
-        url = super().get_host_from_settings(index, variables, servers)
-        return url.replace("https://", "http://")
 
 
 def domino_pinecone3x_init_params(datasource_name: str) -> Dict[str, Union[str, Dict[str, str]]]:
